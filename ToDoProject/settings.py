003668b5461e028b112264504d0b19f31b56f33a
Prerna Sharma <96376454+prerna3101sharma@users.noreply.github.com>
2025-08-07 15:01:19 +0530

    Update settings.py

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     #third party
     'rest_framework',
+    'corsheaders',
 ]
 
 import firebase_admin
@@ -68,12 +69,15 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # for testing only!
 
 ROOT_URLCONF = 'ToDoProject.urls'
 
